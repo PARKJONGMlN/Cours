8eb4582d1c5cd8eb10352bc7c77c5f6871244d84
ParkJongMin0000 <ruho646010@gmail.com>
2023-06-27 23:58:27 +0900

    feat: 갤러리에서 이미지 선택후 이미지 뷰에 표시

--- app/src/main/java/com/pjm/cours/ui/SettingUserInfoActivity.kt
+++ app/src/main/java/com/pjm/cours/ui/SettingUserInfoActivity.kt
@@ -1,16 +1,34 @@
 package com.pjm.cours.ui
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.pjm.cours.databinding.ActivitySettingUserInfoBinding
 
 class SettingUserInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingUserInfoBinding
+    private var isImageSelected = false
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri.let {
+                binding.ivUserProfileImage.load(uri) {
+                    transformations(CircleCropTransformation())
+                }
+                isImageSelected = true
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingUserInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ivUserProfileImage.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 }
