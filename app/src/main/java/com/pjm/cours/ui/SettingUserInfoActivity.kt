dbc47a59a408859b39ebad8784582c6b94cf9c16
ParkJongMin0000 <ruho646010@gmail.com>
2023-06-27 23:59:25 +0900

    feat: 이미지 선택, 닉네임 공백 아닐시 완료 버튼 활성화

--- app/src/main/java/com/pjm/cours/ui/SettingUserInfoActivity.kt
+++ app/src/main/java/com/pjm/cours/ui/SettingUserInfoActivity.kt
@@ -1,9 +1,12 @@
 package com.pjm.cours.ui
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.pjm.cours.databinding.ActivitySettingUserInfoBinding
@@ -12,6 +15,7 @@ class SettingUserInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingUserInfoBinding
     private var isImageSelected = false
+    private var isNicknameEntered = false
     private val getContent =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri.let {
@@ -19,6 +23,7 @@ class SettingUserInfoActivity : AppCompatActivity() {
                     transformations(CircleCropTransformation())
                 }
                 isImageSelected = true
+                checkInputs()
             }
         }
 
@@ -27,8 +32,22 @@ class SettingUserInfoActivity : AppCompatActivity() {
         binding = ActivitySettingUserInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSettingComplete.setOnClickListener{
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
         binding.ivUserProfileImage.setOnClickListener {
             getContent.launch("image/*")
+            Log.d("SettingUserInfoActivity", "onCreate: ")
         }
+
+        binding.etUserNickname.doOnTextChanged { _, _, _, _ ->
+            isNicknameEntered = binding.etUserNickname.text.toString().isNotBlank()
+            checkInputs()
+        }
+    }
+
+    private fun checkInputs() {
+        binding.btnSettingComplete.isEnabled = isImageSelected && isNicknameEntered
     }
 }
