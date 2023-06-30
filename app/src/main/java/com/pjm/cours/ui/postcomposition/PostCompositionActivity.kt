e3bb33f3b633bbebd9f0837f39ffd830dc93ac7c
ParkJongMin0000 <ruho646010@gmail.com>
2023-06-30 15:46:54 +0900

    feat: 모임 생성 화면 - 날짜 선택 구현

--- app/src/main/java/com/pjm/cours/ui/postcomposition/PostCompositionActivity.kt
+++ app/src/main/java/com/pjm/cours/ui/postcomposition/PostCompositionActivity.kt
@@ -2,7 +2,10 @@ package com.pjm.cours.ui.postcomposition
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.pjm.cours.R
 import com.pjm.cours.databinding.ActivityPostCompositionBinding
+import com.pjm.cours.util.DateFormat
 
 class PostCompositionActivity : AppCompatActivity() {
 
@@ -19,6 +22,18 @@ class PostCompositionActivity : AppCompatActivity() {
     private fun setLayout() {
         binding.appBarPostComposition.setNavigationOnClickListener {
             finish()
+        }
+
+        binding.ivSelectDateIcon.setOnClickListener {
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText(R.string.label_post_select_date_message)
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build()
+            datePicker.addOnPositiveButtonClickListener {
+                binding.tvPostSelectedDate.text = DateFormat.convertDisplayDate(it)
+            }
+            datePicker.show(supportFragmentManager,"SELECT_DATE")
         }
     }
 }
---
+++ app/src/main/java/com/pjm/cours/util/DateFormat.kt
@@ -0,0 +1,16 @@
+package com.pjm.cours.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateFormat {
+
+    private const val DATE_YEAR_MONTH_DAY = "yyyy / MM / dd"
+
+    fun convertDisplayDate(long: Long): String {
+        val simpleDateFormat = SimpleDateFormat(DATE_YEAR_MONTH_DAY, Locale.KOREA)
+        val date = Date()
+        date.time = long
+        return simpleDateFormat.format(date)
+    }
+}
