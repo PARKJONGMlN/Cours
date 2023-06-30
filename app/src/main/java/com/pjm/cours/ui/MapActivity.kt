7d88b36f226ed2ab9f39c2d599689b914bc7ab1f
ParkJongMin0000 <ruho646010@gmail.com>
2023-06-30 15:08:33 +0900

    feat: 지도 화면 - 모임 생성 화면으로 이동하는 메뉴 버튼 구현

--- app/src/main/java/com/pjm/cours/ui/MapActivity.kt
+++ app/src/main/java/com/pjm/cours/ui/MapActivity.kt
@@ -1,13 +1,33 @@
 package com.pjm.cours.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pjm.cours.R
+import com.pjm.cours.databinding.ActivityMapBinding
+import com.pjm.cours.ui.postcomposition.PostCompositionActivity
 
 class MapActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMapBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setLayout()
+    }
+
+    private fun setLayout() {
+        binding.appBarPostComposition.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.create_post -> {
+                    startActivity(Intent(this,PostCompositionActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
